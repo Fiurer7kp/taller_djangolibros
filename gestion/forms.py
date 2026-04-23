34e7f4ba4f79c8b501803e788b01a2962021504d
Fiurer7kp <jhonjhoncoral@hotmail.com>
2026-04-23 12:19:13 -0500

    Agregar widgets de formulario con selector de fecha

--- gestion/forms.py
+++ gestion/forms.py
@@ -5,8 +5,28 @@ class AutorForm(forms.ModelForm):
     class Meta:
         model = Autor
         fields = ['nombre', 'correo', 'nacionalidad', 'fecha_nacimiento', 'biografia']
+        widgets = {
+            'fecha_nacimiento': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control'
+            }),
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'correo': forms.EmailInput(attrs={'class': 'form-control'}),
+            'nacionalidad': forms.TextInput(attrs={'class': 'form-control'}),
+            'biografia': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
 
 class LibroForm(forms.ModelForm):
     class Meta:
         model = Libro
         fields = ['titulo', 'fecha_publicacion', 'genero', 'isbn', 'autor']
+        widgets = {
+            'fecha_publicacion': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control'
+            }),
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'genero': forms.TextInput(attrs={'class': 'form-control'}),
+            'isbn': forms.TextInput(attrs={'class': 'form-control'}),
+            'autor': forms.Select(attrs={'class': 'form-control'}),
+        }
